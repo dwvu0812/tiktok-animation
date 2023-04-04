12e7154c3aff9179285d8a87591e0267f83e0185
dwvux0812 <khanhdu1211@gmail.com>
2023-04-04 23:35:41 +0700

    Pan responder

--- App.tsx
+++ App.tsx
@@ -10,8 +10,8 @@ import {
   useBottomTabBarHeight,
 } from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import React, {useState} from 'react';
-import {FlatList, Image, StyleSheet} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {FlatList, Image, PanResponder, StyleSheet, View} from 'react-native';
 import VideoItem from './src/screen/VideoItem';
 import {WINDOW_HEIGHT} from './src/utils/utils';
 import videosData from './src/videosData';
@@ -39,102 +39,16 @@ const HomeScreen = () => {
 const BottomTab = createBottomTabNavigator();
 
 function App(): JSX.Element {
-  return (
-    <NavigationContainer>
-      <BottomTab.Navigator
-        screenOptions={{
-          tabBarStyle: {backgroundColor: '#000'},
-          headerShown: false,
-          tabBarActiveTintColor: 'white',
-        }}>
-        <BottomTab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Home',
-            // eslint-disable-next-line react/no-unstable-nested-components
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={require('./src/assets/images/home.png')}
-                style={[
-                  styles.bottomTabIcon,
-                  focused && styles.bottomTabIconFocused,
-                ]}
-              />
-            ),
-          }}
-        />
-        <BottomTab.Screen
-          name="Discover"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Discover',
-            // eslint-disable-next-line react/no-unstable-nested-components
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={require('./src/assets/images/search.png')}
-                style={[
-                  styles.bottomTabIcon,
-                  focused && styles.bottomTabIconFocused,
-                ]}
-              />
-            ),
-          }}
-        />
-        <BottomTab.Screen
-          name="NewVideo"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: () => null,
-            // eslint-disable-next-line react/no-unstable-nested-components
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={require('./src/assets/images/new-video.png')}
-                style={[
-                  styles.newVideo,
-                  // focused && styles.bottomTabIconFocused,
-                ]}
-              />
-            ),
-          }}
-        />
-        <BottomTab.Screen
-          name="Inbox"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Inbox',
-            // eslint-disable-next-line react/no-unstable-nested-components
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={require('./src/assets/images/message.png')}
-                style={[
-                  styles.bottomTabIcon,
-                  focused && styles.bottomTabIconFocused,
-                ]}
-              />
-            ),
-          }}
-        />
-        <BottomTab.Screen
-          name="Profile"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Profile',
-            // eslint-disable-next-line react/no-unstable-nested-components
-            tabBarIcon: ({focused}) => (
-              <Image
-                source={require('./src/assets/images/user.png')}
-                style={[
-                  styles.bottomTabIcon,
-                  focused && styles.bottomTabIconFocused,
-                ]}
-              />
-            ),
-          }}
-        />
-      </BottomTab.Navigator>
-    </NavigationContainer>
-  );
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: (event, gesture) => {},
+    }),
+  ).current;
+  return <View {...panResponder.panHandlers} style={{flex: 1}} />;
 }
 
 export default App;
